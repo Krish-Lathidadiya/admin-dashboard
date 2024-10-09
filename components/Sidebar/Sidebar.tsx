804a36b55063d3804f62554b048b919d456d3c40
Krish-Lathidadiya <krishlathidadiya.sus@gmail.com>
2024-10-09 22:33:46 +0530

    feat:Add sidebar

---
+++ components/Sidebar/Sidebar.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import AccountToggle from './AccountToggle'
+import Search from './Search'
+import RouteSelect from './RouteSelect.'
+import Plan from './Plan'
+
+function Sidebar() {
+  return (
+    <div >
+        <div className='overflow-y-scroll sticky top-4 h-[calc(100vh-32px-48px)]'>
+            {/* Main sidebar content */}
+            <AccountToggle/>
+            <Search/>
+            <RouteSelect/>
+        </div>
+        {/* Plan toggle */}
+        <Plan/>
+    </div>
+  )
+}
+
+export default Sidebar
